8d251a260280b99d9dbd45aba13a0307d5ab71f5
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-03 11:18:56 +0100

    ajout du menu de création de compte et de connexion

---
+++ frontend/src/app/creer-compte/creer-compte.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-creer-compte',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './creer-compte.html',
+  styleUrl: './creer-compte.css',
+})
+export class CreerCompte {
+
+}
