a87cfc8de96416e299778543a22eede47e323cef
Barth <derouaultbarthelemy@protonmail.com>
2026-02-10 11:46:18 +0100

    changement de l'endpoint pour configurer

--- frontend/src/app/configurer-compte/configurer-compte.ts
+++ frontend/src/app/configurer-compte/configurer-compte.ts
@@ -19,7 +19,7 @@ export class ConfigurerCompte {
   constructor(private http: HttpClient, private router: Router) { }
 
   configurer() {
-    this.http.post('http://127.0.0.1:5000/user/configurer', {
+    this.http.post('http://127.0.0.1:5000/user/option/configurer', {
       taille: this.height,
       date_naissance: this.birthDate
     }).subscribe({
