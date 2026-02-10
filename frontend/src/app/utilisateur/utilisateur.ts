862c63d16b2535ad2e9f01f7a6004482b5ac91d3
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-10 12:05:17 +0100

    modification date de naissance et taille

--- frontend/src/app/utilisateur/utilisateur.ts
+++ frontend/src/app/utilisateur/utilisateur.ts
@@ -2,12 +2,14 @@ import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs/operators';
 import { isPlatformBrowser } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-utilisateur',
-  imports: [FormsModule, RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './utilisateur.html',
   styleUrl: './utilisateur.css',
 })
@@ -25,6 +27,8 @@ export class Utilisateur {
   birthDate = '';
   BbirthDate = '';
 
+  constructor(private router: Router, private cdr: ChangeDetectorRef) { }
+
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.getAllinformation();
@@ -39,9 +43,35 @@ export class Utilisateur {
       console.log('User:', res);
 
     });
+
   }
 
   modif() {
+    this.http.post('http://127.0.0.1:5000/user/option/configurer', {
+      date_naissance: this.birthDate,
+      taille: this.taille
+
+    }).subscribe({
+
+      next: (res: any) => {
+        console.log('RESPONSE OK', res);
+        this.backendResponse = "modifications appliquées";
+        this.cdr.detectChanges();
+      },
+
+      error: (err: any) => {
+        // erreurs HTTP (400, 409, 500…)
+        if (err.error && err.error.message) {
+          this.backendResponse = err.error.message; // <- message du backend
+          this.cdr.detectChanges();
+        } else {
+          this.backendResponse = 'Erreur serveur';
+        }
+      }
+    });
+
+
+
 
   }
 
