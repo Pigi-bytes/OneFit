5ae85fc168c95687c35ac699f723c1f989c720ef
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-10 17:31:26 +0100

    ont peut ajouter / modifier des poids

--- frontend/src/app/menu-poid/menu-poid.ts
+++ frontend/src/app/menu-poid/menu-poid.ts
@@ -1,11 +1,73 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-menu-poid',
-  imports: [],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './menu-poid.html',
   styleUrl: './menu-poid.css',
 })
 export class MenuPoid {
 
+  poid = '';
+  pDate = '';
+  backendResponse = '';
+  note: string | null = null;
+
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  ajouterPoid() {
+
+    if (!this.note || this.note.trim() === '') {
+      this.note = null;
+    }
+
+    this.http.post('http://127.0.0.1:5000/user/ajouterOuModifierPoids', {
+      date: this.pDate,
+      poids: this.poid,
+      note: this.note
+    }).subscribe({
+      next: (res: any) => {
+        console.log('RESPONSE OK', res);
+        this.backendResponse = res.message;
+        this.cdr.detectChanges();
+
+      },
+      error: (err: any) => {
+        //erreur 422
+        if (err.error.code == 422 && err.error?.errors) {
+
+          const errorsObj = err.error.errors;
+          const messages: string[] = [];
+
+          for (const key in errorsObj) {
+
+            const value = errorsObj[key];
+            Object.values(value).forEach(v => {
+              if (Array.isArray(v)) messages.push(...v);
+              else if (typeof v === 'string') messages.push(v);
+              messages.push("\n");
+            });
+          }
+
+          this.backendResponse = messages.join('\n');
+        }
+        // erreurs HTTP (400, 409, 500…)
+        else if (err.error && err.error.message) {
+          this.backendResponse = err.error.message; // <- message du backend
+        } else {
+          this.backendResponse = 'Erreur serveur';
+        }
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
 }
+
+
