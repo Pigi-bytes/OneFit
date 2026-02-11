50b2c989560a54ac79c6241ed47113c91039da9d
tkmeriem <Meriem.Taieb_Kherafa.Etu@univ-lemans.fr>
2026-02-11 14:54:00 +0100

    sauvegarde du thème (light/dark) choisi dans localStorage

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Theme } from '../theme';
 
@@ -9,11 +9,24 @@ import { Theme } from '../theme';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
+export class Home implements OnInit {
+  isDark = false;
   constructor (private theme: Theme){}
+
+  ngOnInit() {
+    if (typeof localStorage !== 'undefined') {
+      if(localStorage.getItem('darkMode') === 'true'){
+        document.body.classList.add('dark');
+      }
+    }
+  }
 
   toggleTheme(){
     this.theme.toggleDark();
+    this.isDark = document.body.classList.contains('dark');
+
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('darkMode', String(this.isDark));
+    }
   }
 }
