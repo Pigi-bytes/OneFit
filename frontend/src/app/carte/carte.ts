5a2f48c182de457c6ba317fa16a69e71ddf5aabe
Baptcarf <baptiste.carfantan72@gmail.com>
2026-03-06 13:43:11 +0100

    ajout de l'icon en couleur

--- frontend/src/app/carte/carte.ts
+++ frontend/src/app/carte/carte.ts
@@ -6,6 +6,8 @@ import { ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { marker, Marker } from 'leaflet';
+import { from } from 'rxjs';
 
 
 @Component({
@@ -25,6 +27,9 @@ export class Carte implements AfterViewInit {
 
     ville = "";
     backendResponse = "";
+
+
+    markers: L.Marker[] = [];
 
     constructor(@Inject(PLATFORM_ID) private platformId: Object, private not: Notification, private cdr: ChangeDetectorRef, private http: HttpClient) { }
 
@@ -49,7 +54,13 @@ export class Carte implements AfterViewInit {
 
         }
     }
+
     trouverLoc() {
+        const myCustomColour = 'red';
+
+        const markerHtmlStyles = `
+            background-color: ${myCustomColour};
+     `
         console.log("HTTP:", this.http);
 
         this.http.post('http://127.0.0.1:5000/user/option/salle', {
@@ -59,36 +70,53 @@ export class Carte implements AfterViewInit {
             next: async (res: any) => {
                 console.log('RESPONSE OK', res);
 
+                this.markers.forEach(m => { m.remove() })
+                this.markers = [];
+
 
                 res.results.forEach((salle: any) => {
-                    const lat = salle.latitude;
-                    const lng = salle.longitude;
+                    const lat = Number(salle.latitude);
+                    const lng = Number(salle.longitude);
                     const name = salle.name || 'N/A';
                     const adresse = salle.location.address || 'N/A';
                     const tel = salle.tel || 'N/A';
                     const mail = salle.mail || 'N/A';
                     const webSite = salle.website || 'N/A';
-                    const customIcon = this.L.icon({
-                        iconUrl: `${salle.categories[0].icon.prefix}64${salle.categories[0].icon.suffix}`,
-                        iconSize: [64, 64], // taille de l'icône
-                        iconAnchor: [16, 32], // point de l'icône qui correspond à la position du marker
-                        popupAnchor: [0, -32] // point d'où le popup s'ouvre
+                    const customIcon = this.L.divIcon({
+                        className: '',
+                        html: `<div style="
+                            width: 64px; height: 64px; 
+                            background-image: url('${salle.categories[0].icon.prefix}64${salle.categories[0].icon.suffix}');
+                            background-size: cover;
+                            filter: brightness(0) saturate(100%) invert(13%) sepia(90%) saturate(7495%) hue-rotate(350deg) brightness(1) contrast(1);
+                            border-radius: 50%;
+                        "></div>`,
+                        iconSize: [64, 64],
+                        iconAnchor: [16, 32],
+                        popupAnchor: [0, -32]
                     });
 
+                    const marker = this.L.marker([lat, lng], { icon: customIcon }).addTo(this.map).bindPopup(`<ul>
+                        <li>nom : ${name} </li>
+                        <li> adresse :${adresse} </li>
+                        <li> téléphone :${tel} </li>
+                        <li> mail : ${mail} </li>
+                        <li> site : ${webSite} </li>
+                         </ul>`);
 
-                    this.L.marker([lat, lng], { icon: customIcon }).addTo(this.map).bindPopup(`<ul>
-                        <li>nom : ${name}</li>
-                        <li>adresse :${adresse}</li>
-                        <li>téléphone :${tel}</li>
-                        <li>mail : ${mail}</li>
-                        <li>site : ${webSite}</li>
-                        </ul>`);
+                    this.markers.push(marker);
                 });
 
-                const group = this.L.featureGroup(
-                    res.results.map((salle: any) => this.L.marker([salle.latitude, salle.longitude]))
-                );
-                this.map.fitBounds(group.getBounds(), { padding: [50, 50] });
+                if (this.markers.length === 1) {
+                    this.map.setView(this.markers[0].getLatLng(), 14);
+                } else {
+                    const bounds = this.L.latLngBounds([]);
+                    this.markers.forEach(m => bounds.extend(m.getLatLng()));
+                    this.map.fitBounds(bounds, { padding: [50, 50] });
+                }
+
+
+
 
                 this.cdr.detectChanges();
             },
