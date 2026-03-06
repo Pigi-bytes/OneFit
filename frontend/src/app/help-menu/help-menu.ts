5ae8ad76330f8828f7237f251fef7838be70f737
tkmeriem <Meriem.Taieb_Kherafa.Etu@univ-lemans.fr>
2026-03-06 16:29:55 +0100

    création du menu d'aide pour les exos (jai juste fait ng g c y'a rien dedans)

---
+++ frontend/src/app/help-menu/help-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-menu',
+  imports: [],
+  templateUrl: './help-menu.html',
+  styleUrl: './help-menu.css',
+})
+export class HelpMenu {
+
+}
