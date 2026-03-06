941c1772c5cfa98808591b7c6bd946168b3f1c70
Baptcarf <baptiste.carfantan72@gmail.com>
2026-03-06 16:33:12 +0100

    ajout de la requét à l'api de sport

--- frontend/src/app/creer-seance/creer-seance.ts
+++ frontend/src/app/creer-seance/creer-seance.ts
@@ -4,12 +4,12 @@ import { RouterModule, Router } from '@angular/router';
 import { Notification } from '../notification';
 import { ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-creer-seance',
-    imports: [FormsModule, HttpClientModule, CommonModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './creer-seance.html',
     styleUrl: './creer-seance.css',
 })
@@ -19,7 +19,61 @@ export class CreerSeance {
 
     constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef, private not: Notification) { }
 
-    trouverExo() { }
+    trouverExo() {
+        this.http.post('http://127.0.0.1:5000/externe/searchExo', {
+            recherche: this.nom,
+
+        }).subscribe({
+
+            next: (res: any) => {
+                console.log('RESPONSE OK', res);
+
+
+                res.results.forEach((exo: any) => {
+                    const nom = String(exo[0]);
+                    const img = String(exo[2]);
+
+                    console.log(nom);
+                });
+
+
+                this.backendResponse = res.message;
+                this.cdr.detectChanges();
+
+            },
+
+            error: (err: any) => {
+                //erreur 422
+                if (err.status == 422 && err.error.errors) {
+
+                    const errorsObj = err.error.errors;
+                    const messages: string[] = [];
+
+
+
+                    for (const key in errorsObj) {
+
+                        const value = errorsObj[key];
+                        Object.values(value).forEach(v => {
+                            if (Array.isArray(v)) messages.push(...v);
+                            else if (typeof v === 'string') messages.push(v);
+                        });
+                    }
+
+                    this.backendResponse = messages.join('\n');
+                }
+                // erreurs HTTP (400, 409, 500…)
+                else if (err.error && err.error.message) {
+                    this.backendResponse = err.error.message; // <- message du backend
+                } else {
+                    this.backendResponse = 'Erreur serveur';
+                }
+                this.cdr.detectChanges();
+            }
+        });
+
+    }
+
 
 
     resetNotif() {
