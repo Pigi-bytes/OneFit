03c36c8f6164f7e80312417912b5f7c87164749d
Baptcarf <baptiste.carfantan72@gmail.com>
2026-03-06 15:56:40 +0100

    fixe bug visuel

--- frontend/src/app/creer-seance/creer-seance.ts
+++ frontend/src/app/creer-seance/creer-seance.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { Notification } from '../notification';
+import { ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-creer-seance',
-  imports: [],
-  templateUrl: './creer-seance.html',
-  styleUrl: './creer-seance.css',
+    selector: 'app-creer-seance',
+    imports: [FormsModule, HttpClientModule, CommonModule],
+    templateUrl: './creer-seance.html',
+    styleUrl: './creer-seance.css',
 })
 export class CreerSeance {
+    backendResponse = "";
+    nom = "";
+
+    constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef, private not: Notification) { }
+
+    trouverExo() { }
+
+
+    resetNotif() {
+        this.not.reset(this, this.cdr);
+    }
 
 }
