2287ad32ed2f3f59825ac71e4b599f196b9e3d6a
tkmeriem <Meriem.Taieb_Kherafa.Etu@univ-lemans.fr>
2026-03-09 15:09:43 +0100

    affichage de détails pour l'exo (image et nom)

--- frontend/src/app/afficher-exo/afficher-exo.ts
+++ frontend/src/app/afficher-exo/afficher-exo.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { RouterModule,Router } from '@angular/router';
+import { Notification } from '../notification';
+import { ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-afficher-exo',
-  imports: [RouterModule],
-  templateUrl: './afficher-exo.html',
-  styleUrl: './afficher-exo.css',
+    selector: 'app-afficher-exo',
+    imports: [RouterModule,CommonModule],
+    templateUrl: './afficher-exo.html',
+    styleUrl: './afficher-exo.css',
 })
 export class AfficherExo {
+    constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef, private not: Notification) {}
+    backendResponse = "";
+    id = 'exr_41n2hxnFMotsXTj3'
+	exo:  any;
 
+    ngOnInit() {
+            this.http.post('http://127.0.0.1:5000/externe/getExo', {
+                exoId: this.id,
+
+            }).subscribe({
+
+                next: (res: any) => {
+                    console.log('RESPONSE OK', res);
+
+                    this.exo = res;
+                    console.log(this.exo);
+
+                    this.backendResponse = res.message;
+                    this.cdr.detectChanges();
+                },
+
+                error: (err: any) => {
+                    //erreur 422
+                    if (err.status == 422 && err.error.errors) {
+
+                        const errorsObj = err.error.errors;
+                        const messages: string[] = [];
+
+                        for (const key in errorsObj) {
+
+                            const value = errorsObj[key];
+                            Object.values(value).forEach(v => {
+                                if (Array.isArray(v)) messages.push(...v);
+                                else if (typeof v === 'string') messages.push(v);
+                            });
+                        }
+
+                        this.backendResponse = messages.join('\n');
+                    }
+                    // erreurs HTTP (400, 409, 500…)
+                    else if (err.error && err.error.message) {
+                        this.backendResponse = err.error.message; // <- message du backend
+                    } else {
+                        this.backendResponse = 'Erreur serveur';
+                    }
+                    this.cdr.detectChanges();
+                }
+            });
+
+        }
 }
