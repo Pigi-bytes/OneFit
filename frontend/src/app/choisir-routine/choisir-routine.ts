047a2eaa3b427af8d1c34b7ab08c608307e7bbff
tkmeriem <Meriem.Taieb_Kherafa.Etu@univ-lemans.fr>
2026-03-11 10:20:37 +0100

    created components: seance, choisir-routine

---
+++ frontend/src/app/choisir-routine/choisir-routine.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choisir-routine',
+  imports: [],
+  templateUrl: './choisir-routine.html',
+  styleUrl: './choisir-routine.css',
+})
+export class ChoisirRoutine {
+
+}
