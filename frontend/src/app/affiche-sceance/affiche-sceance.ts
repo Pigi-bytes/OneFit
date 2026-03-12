52290dbe44f9de11b8edcc6b8f52fffcb8749c0c
Baptcarf <baptiste.carfantan72@gmail.com>
2026-03-12 20:34:32 +0100

    affichage de la séance du jour choisit

--- frontend/src/app/affiche-sceance/affiche-sceance.ts
+++ frontend/src/app/affiche-sceance/affiche-sceance.ts
@@ -45,7 +45,49 @@ export class AfficheSceance implements OnInit {
     }
 
     chargeSeance() {
-        console.log("test");
+        this.http.post('http://127.0.0.1:5000/sport/getSeanceDuJour', {
+            routine_id: -1,
+            day: this.jour
+
+        }).subscribe({
+
+            next: (res: any) => {
+                console.log('RESPONSE OK', res);
+                this.exercices = res.seance.exercises;
+
+                this.backendResponse = res.message;
+                this.cdr.detectChanges();
+            },
+
+            error: (err: any) => {
+                //erreur 422
+                if (err.status == 422 && err.error.errors) {
+
+                    const errorsObj = err.error.errors;
+                    const messages: string[] = [];
+
+                    for (const key in errorsObj) {
+
+                        const value = errorsObj[key];
+                        Object.values(value).forEach(v => {
+                            if (Array.isArray(v)) messages.push(...v);
+                            else if (typeof v === 'string') messages.push(v);
+                        });
+                    }
+
+                    this.backendResponse = messages.join('\n');
+                }
+                // erreurs HTTP (400, 409, 500…)
+                else if (err.error && err.error.message) {
+                    this.backendResponse = err.error.message; // <- message du backend
+                } else {
+                    this.backendResponse = 'Erreur serveur';
+                }
+                this.cdr.detectChanges();
+            }
+        });
+
+
     }
 
     modifierExo(id: any) {
