cf715ad23b91178ecd8cae494b8c69dabee9f7a0
Barth <derouaultbarthelemy@protonmail.com>
2026-03-15 00:44:21 +0100

    Afficher routine et renommage de cette derniere

--- frontend/src/app/afficher-routine/afficher-routine.ts
+++ frontend/src/app/afficher-routine/afficher-routine.ts
@@ -6,11 +6,12 @@ import { ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnvoyerElt } from '../envoyerElt'
 import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-afficher-routine',
     standalone: true,
-    imports: [RouterModule, CommonModule],
+    imports: [RouterModule, CommonModule, FormsModule],
     templateUrl: './afficher-routine.html',
     styleUrl: './afficher-routine.css',
 })
@@ -18,14 +19,19 @@ export class AfficherRoutine {
     constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef, private not: Notification, private ei: EnvoyerElt) { }
     backendResponse = "";
     id = null;
+    routine: any;
+    routine_nom = "";
     seances: any;
     private subscription?: Subscription;
+    is_renaming = false;
 
     ngOnInit() {
         this.seances = null;
         this.subscription = this.ei.afficheExercice$.subscribe((id) => {
             this.modifId(id);
-            this.chargeRoutine();
+            this.chargerInfosRoutines();
+            this.chargerSeances();
+            this.is_renaming = false;
         });
     }
 
@@ -33,7 +39,48 @@ export class AfficherRoutine {
         this.id = id.toString();
     }
 
-    chargeRoutine() {
+    chargerInfosRoutines(){
+        this.http.post('http://127.0.0.1:5000/sport/getRoutine', {
+            routine_id: this.id,
+        }).subscribe({
+
+            next: (res: any) => {
+                console.log('RESPONSE OK', res);
+                this.routine = res;
+                this.backendResponse = res.message;
+                this.cdr.detectChanges();
+            },
+
+            error: (err: any) => {
+                //erreur 422
+                if (err.status == 422 && err.error.errors) {
+
+                    const errorsObj = err.error.errors;
+                    const messages: string[] = [];
+
+                    for (const key in errorsObj) {
+
+                        const value = errorsObj[key];
+                        Object.values(value).forEach(v => {
+                            if (Array.isArray(v)) messages.push(...v);
+                            else if (typeof v === 'string') messages.push(v);
+                        });
+                    }
+
+                    this.backendResponse = messages.join('\n');
+                }
+                // erreurs HTTP (400, 409, 500…)
+                else if (err.error && err.error.message) {
+                    this.backendResponse = err.error.message; // <- message du backend
+                } else {
+                    this.backendResponse = 'Erreur serveur';
+                }
+                this.cdr.detectChanges();
+            }
+        });
+    }
+
+    chargerSeances() {
         this.http.post('http://127.0.0.1:5000/sport/getSeancesPrevu', {
             routine_id: this.id,
         }).subscribe({
@@ -160,4 +207,52 @@ export class AfficherRoutine {
 
         }
     }
+
+    ouvrirFormulaireRenommerRoutine(){
+        this.is_renaming = true;
+        this.cdr.detectChanges();
+    }
+
+    renommerRoutine(){
+        this.http.post('http://127.0.0.1:5000/sport/modiferNomRoutine', {
+            routine_id: this.id,
+            name: this.routine_nom
+        }).subscribe({
+            next: (res: any) => {
+                this.routine.name = res.name ?? this.routine_nom;
+                this.is_renaming = false;
+                this.backendResponse = res.message;
+                this.ei.triggerRefresh(null);
+                this.cdr.detectChanges();
+            },
+
+            error: (err: any) => {
+                //erreur 422
+                if (err.status == 422 && err.error.errors) {
+
+                    const errorsObj = err.error.errors;
+                    const messages: string[] = [];
+
+                    for (const key in errorsObj) {
+
+                        const value = errorsObj[key];
+                        Object.values(value).forEach(v => {
+                            if (Array.isArray(v)) messages.push(...v);
+                            else if (typeof v === 'string') messages.push(v);
+                        });
+                    }
+
+                    this.backendResponse = messages.join('\n');
+                }
+                // erreurs HTTP (400, 409, 500…)
+                else if (err.error && err.error.message) {
+                    this.backendResponse = err.error.message; // <- message du backend
+                } else {
+                    this.backendResponse = 'Erreur serveur';
+                }
+                this.cdr.detectChanges();
+            }
+        });
+    }
+    
 }
