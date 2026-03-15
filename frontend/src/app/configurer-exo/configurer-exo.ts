1cee682f10c7a8ccdc144f75647869aacda6e33e
Baptcarf <baptiste.carfantan72@gmail.com>
2026-03-15 16:16:35 +0100

    ajout de la supression d'un exercice de la séance

--- frontend/src/app/configurer-exo/configurer-exo.ts
+++ frontend/src/app/configurer-exo/configurer-exo.ts
@@ -207,6 +207,55 @@ export class ConfigurerExo {
         }
     }
 
+    supprimer() {
+        const confirmAction = confirm("Voulez-vous vraiment supprimer cette exercice de la séance ?");
+
+        if (confirmAction) {
+            this.http.delete('http://127.0.0.1:5000/sport/supprimerExoSeance', {
+                body: {
+                    routine_id: -1,
+                    day: this.jour,
+                    seance_exercise_id: this.idSequence,
+                }
+            }).subscribe({
+                next: (res: any) => {
+                    console.log('RESPONSE OK', res);
+                    this.backendResponse = res.message;
+                    this.ei.triggerRefresh([Message.AFFICHER_SEANCE, null]);
+                    this.annuler();
+                },
+
+                error: (err: any) => {
+                    //erreur 422
+                    if (err.status == 422 && err.error.errors) {
+
+                        const errorsObj = err.error.errors;
+                        const messages: string[] = [];
+
+                        for (const key in errorsObj) {
+
+                            const value = errorsObj[key];
+                            Object.values(value).forEach(v => {
+                                if (Array.isArray(v)) messages.push(...v);
+                                else if (typeof v === 'string') messages.push(v);
+                            });
+                        }
+
+                        this.backendResponse = messages.join('\n');
+                    }
+                    // erreurs HTTP (400, 409, 500…)
+                    else if (err.error && err.error.message) {
+                        this.backendResponse = err.error.message; // <- message du backend
+                    } else {
+                        this.backendResponse = 'Erreur serveur';
+                    }
+                    this.cdr.detectChanges();
+                }
+            });
+        }
+
+    }
+
     ngOnDestroy() {
         this.subscription?.unsubscribe();
     }
